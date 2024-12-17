40ef82ea5f64ad1ef7b7f72860d615652c575621
Lakshay <lakshayyadavmain@gmail.com>
2024-12-17 17:11:47 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['assets.aceternity.com'], // Add the external domain here
+  },
   /* config options here */
 };
 
